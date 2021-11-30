676d688198b65d6beeb5916dd229f0e0b3107c7a
Mario Milunović <mario.milunovic@gmail.com>
2021-11-30 23:26:38 +0100

    Fixing Tailwind...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
 
-    npmpurge: [
+    purge: [
 
-        // './storage/framework/views/*.php',
-        // './resources/**/*.blade.php',
-        // './resources/**/*.js',
-        // './resources/**/*.vue',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
 
     ],
      darkMode: false, // or 'media' or 'class'
