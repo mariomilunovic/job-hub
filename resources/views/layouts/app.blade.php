c4011862967f94e07a4355039aa5667090a25831
Mario Milunovic <mario.milunovic@gmail.com>
2021-11-30 18:38:01 +0100

    Tailwind not working in production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <style>
+    {{-- <style>
 
         /* Layout large screens */
         @media screen and (min-width: 760px)
@@ -41,67 +41,68 @@
                 grid-template-areas:
                 "header header"
                 "navbar main"
-                "footer footer"     }
+                "footer footer"
             }
+        }
 
-            /* Layout for small screens */
-            @media screen and (max-width: 760px)
+        /* Layout for small screens */
+        @media screen and (max-width: 760px)
+        {
+            #container
             {
-                #container
-                {
-                    display: grid;
-                    grid-template-columns: 1fr;
-                    grid-template-rows: 1fr 3fr 6fr 1fr;
-                    grid-gap: 12px;
-                    max-width: 80vw;
-                    min-height: 100vh;
-                    margin: 0 auto;
-                    grid-template-areas:
-                    "header"
-                    "navbar"
-                    "main"
-                    "footer"
-                }
+                display: grid;
+                grid-template-columns: 1fr;
+                grid-template-rows: 1fr 3fr 6fr 1fr;
+                grid-gap: 12px;
+                max-width: 80vw;
+                min-height: 100vh;
+                margin: 0 auto;
+                grid-template-areas:
+                "header"
+                "navbar"
+                "main"
+                "footer"
             }
+        }
 
-            header{
-                grid-area: header;
-            }
+        header{
+            grid-area: header;
+        }
 
-            nav{
-                grid-area: navbar;
-            }
+        nav{
+            grid-area: navbar;
+        }
 
-            main{
-                grid-area: main;
-            }
+        main{
+            grid-area: main;
+        }
 
-            footer{
-                grid-area: footer;
-            }
+        footer{
+            grid-area: footer;
+        }
 
-        </style>
+    </style> --}}
 
-        @livewireStyles
+    @livewireStyles
 
-    </head>
+</head>
 
-    <body class="bg-gray-400">
+<body class="bg-gray-400">
 
-        <section id="container" class="bg-gray-400">
+    <section id="container" class="bg-gray-400">
 
-            <header class="card flex-row place-content-between items-center bg-gray-500 mt-3">@include('sections.header')</header>
+        <header class="card flex-row place-content-between items-center bg-gray-500 mt-3">@include('sections.header')</header>
 
-            <nav class="card flex-col bg-blue-500">@include('sections.navbar')</nav>
+        <nav class="card flex-col bg-blue-500">@include('sections.navbar')</nav>
 
-            <main class="card bg-gray-200">@yield('content')</main>
+        <main class="card bg-gray-200">@yield('content')</main>
 
-            <footer class="card flex-row place-content-center items-center bg-gray-500 mb-3">@include('sections.footer')</footer>
+        <footer class="card flex-row place-content-center items-center bg-gray-500 mb-3">@include('sections.footer')</footer>
 
-        </section>
+    </section>
 
-        @livewireScripts
+    @livewireScripts
 
-    </body>
+</body>
 
-    </html>
+</html>
